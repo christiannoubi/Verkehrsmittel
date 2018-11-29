ca390fbaf4622e1e4af23da02355b7f1814d87ba
Christian <noubichristian@yahoo.fr>
2018-11-29 15:39:20 +0100

    complete programm

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ int main() {
     Position pp1 (2,5);
     Position pp2 (5,6);
     Position pp3 (5,8);
+
     Pkw* p1 = new Pkw("Mazda 3", 5, 40, pp);
     Pkw p2 ("Mazda 3", 5, 40, pp);
     Pkw* p3 = new Pkw("Toyota", 6, 70, pp3);
@@ -22,8 +23,6 @@ int main() {
     verkehrsmittelSystem.fuegehinzu(p2);
     verkehrsmittelSystem.fuegehinzu(*p3);
     verkehrsmittelSystem.fuegehinzu(p4);
-
-
 
     Luftfahrzeuge *l = new Luftfahrzeuge("Boeing 747", 30000, pp);
     Luftfahrzeuge *l1 = new Luftfahrzeuge("AirBus A-380", 35000, pp1);
@@ -34,7 +33,6 @@ int main() {
     verkehrsmittelSystem.fuegehinzu(*l2);
     verkehrsmittelSystem.fuegehinzu(*l3);
 
-
     Radfahrzeuge *r = new Radfahrzeuge("VTT",30,pp);
     Radfahrzeuge *r1 = new Radfahrzeuge("Moukong", 40, pp1);
     Radfahrzeuge *r2 = new Radfahrzeuge("Moukouagne", 50, pp2);
@@ -43,33 +41,22 @@ int main() {
     verkehrsmittelSystem.fuegehinzu(*r1);
     verkehrsmittelSystem.fuegehinzu(*r2);
     verkehrsmittelSystem.fuegehinzu(*r3);
+
     int i =1;
     cout << "Die verkehrsmitteln:\n";
     cout << "---------------------Pkw: --------------------\n";
-   /* for ( i = 1; i< ;i++) {
-        cout << " " << i << ". "<< pkw.text()<<"\n";
-        i++;
-
-    }*/
     cout << " " << i++ << ". "<< p1->text()<<"\n";
     cout << " " << i++ << ". "<< p2.text()<<"\n";
     cout << " " << i++ << ". "<< p3->text()<<"\n";
     cout << " " << i++ << ". "<< p4.text()<<"\n";
     cout << "--------------Radfahrzeuge: --------------------\n";
-   /* for (auto rad: verkehrsmittelSystem ) {
-        cout << " " << i << ". "<< rad.text()<<"\n";
-        i++;
-    }*/
+
     cout << " " << i++ << ". "<< l->text()<<"\n";
     cout << " " << i++ << ". "<< l1->text()<<"\n";
     cout << " " << i++ << ". "<< l2->text()<<"\n";
     cout << " " << i++ << ". "<< l3->text()<<"\n";
 
     cout << "--------------Luftfahrzeuge: --------------------\n";
-   /* for (auto luft: verkehrsmittelSystem.Luftfahrzeuge() ) {
-        cout << " " << i << ". "<< luft.text()<<"\n";
-        i++;
-    }*/
     cout << " " << i++ << ". "<< r->text()<<"\n";
     cout << " " << i++ << ". "<< r1->text()<<"\n";
     cout << " " << i++ << ". "<< r2->text()<<"\n";
